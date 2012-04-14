d9bad70a50eb0d539bb11eea69ca9197219c6991
Jonathan Lilliemarck <jonathan.lilliemarck@gmail.com>
2012-04-14 22:06:30 +0200

    Factor node building into a separate class

--- src/texttree/parser.cpp
+++ src/texttree/parser.cpp
@@ -14,18 +14,58 @@ syntax_error::syntax_error(std::string const& what) : std::runtime_error(what)
 
 namespace
 {
-    void text(std::stack<node_ptr,std::vector<node_ptr>>& stack,
+    class tree_builder
+    {
+    public:
+        tree_builder()
+        {
+            stack_.push(std::make_shared<node>());
+        }
+
+        void begin_node(std::string const& text)
+        {
+            node_ptr child = std::make_shared<node>(text);
+            stack_.top()->append_child(child);
+            stack_.push(child);
+        }
+
+        void end_node()
+        {
+            stack_.pop();
+        }
+
+        node_ptr const tree() const
+        {
+            assert(stack_.size() == 1);
+            return stack_.top();
+        }
+
+    private:
+        std::stack<node_ptr,std::vector<node_ptr>> stack_;
+    };
+
+    typedef std::stack<bool,std::vector<bool>> bool_stack;
+
+    void text(bool_stack& is_open, tree_builder& builder,
               std::string::const_iterator const& begin,
               std::string::const_iterator const& end)
     {
-        stack.top()->append_child(std::make_shared<node>(std::string(begin, end)));
+        if (!is_open.top())
+        {
+            is_open.pop();
+            builder.end_node();
+        }
+
+        is_open.push(false);
+        builder.begin_node(std::string(begin, end));
     }
 }
 
 node_ptr const parse_children(std::string const& string)
 {
-    std::stack<node_ptr,std::vector<node_ptr>> stack;
-    stack.push(std::make_shared<node>());
+    bool_stack is_open;
+    is_open.push(true);
+    tree_builder builder;
 
     for (auto iterator = begin(string); iterator != end(string);)
     {
@@ -42,30 +82,36 @@ node_ptr const parse_children(std::string const& string)
                 throw syntax_error("Unterminated quoted string");
             }
 
-            text(stack, iterator, end_of_string);
+            text(is_open, builder, iterator, end_of_string);
             iterator = end_of_string;
         }
         else if (*iterator == '(')
         {
-            auto const& parent_node = stack.top();
-            size_t parent_node_child_count = parent_node->child_count();
-
-            if (parent_node_child_count == 0)
+            if (is_open.top())
             {
                  throw syntax_error("Unexpected (");
             }
 
-            auto const& previous_node = parent_node->child_at(parent_node_child_count - 1);
-            stack.push(previous_node);
+            is_open.top() = true;
         }
         else if (*iterator == ')')
         {
-            if (stack.size() < 2)
+            if (!is_open.top())
+            {
+                is_open.pop();
+                builder.end_node();
+            }
+
+            if (is_open.size() == 1)
             {
                 throw syntax_error("Unexpected )");
             }
 
-            stack.pop();
+            if (is_open.top())
+            {
+                is_open.pop();
+                builder.end_node();
+            }
         }
         else if (!isspace(*iterator))
         {
@@ -79,7 +125,7 @@ node_ptr const parse_children(std::string const& string)
                 return isspace(c) || c == '(' || c == ')';
             });
 
-            text(stack, iterator, end_of_string);
+            text(is_open, builder, iterator, end_of_string);
             iterator = end_of_string;
             continue;
         }
@@ -87,12 +133,17 @@ node_ptr const parse_children(std::string const& string)
         ++iterator;
     }
 
-    if (stack.size() > 1)
+    if (is_open.size() >= 2 && is_open.top())
     {
         throw syntax_error("Missing )");
     }
 
-    return stack.top();
+    if (is_open.size() == 2 && !is_open.top())
+    {
+        builder.end_node();
+    }
+
+    return builder.tree();
 }
 
 node_ptr const parse(std::string const& string)
