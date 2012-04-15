46fbbd9d5370aad2bb557e425332c280e722e5a8
Jonathan Lilliemarck <jonathan.lilliemarck@gmail.com>
2012-04-15 16:47:42 +0200

    Add functions to mutate and iterate nodes

--- src/texttree/node.cpp
+++ src/texttree/node.cpp
@@ -15,6 +15,11 @@ std::string const node::text() const
     return text_;
 }
 
+void node::set_text(std::string const& text)
+{
+    text_ = text;
+}
+
 void node::append_child(node_ptr const& child)
 {
     children_.push_back(child);
@@ -28,6 +33,39 @@ size_t const node::child_count() const
 node_ptr const node::child_at(size_t index) const
 {
     return children_[index];
+}
+
+node::iterator const node::begin()
+{
+    return children_.begin();
+}
+
+node::const_iterator const node::begin() const
+{
+    return children_.begin();
+}
+
+node::iterator const node::end()
+{
+    return children_.end();
+}
+
+node::const_iterator const node::end() const
+{
+    return children_.end();
+}
+
+node_ptr const find(node const& node, std::string const& text)
+{
+    for (auto const& child : node)
+    {
+        if (child->text() == text)
+        {
+            return child;
+        }
+    }
+
+    return {};
 }
 
 } // namespace tt
--- src/texttree/node.hpp
+++ src/texttree/node.hpp
@@ -12,20 +12,30 @@ typedef std::shared_ptr<class node> node_ptr;
 class node
 {
 public:
+    typedef std::vector<node_ptr>::iterator iterator;
+    typedef std::vector<node_ptr>::const_iterator const_iterator;
+
     node();
     node(std::string const& text);
 
     std::string const text() const;
+    void set_text(std::string const& text);
 
     void append_child(node_ptr const& child);
     size_t const child_count() const;
     node_ptr const child_at(size_t index) const;
 
+    iterator const begin();
+    const_iterator const begin() const;
+    iterator const end();
+    const_iterator const end() const;
+
 private:
     std::string text_;
     std::vector<node_ptr> children_;
-
 };
+
+node_ptr const find(node const& node, std::string const& text);
 
 } // namespace tt
 
